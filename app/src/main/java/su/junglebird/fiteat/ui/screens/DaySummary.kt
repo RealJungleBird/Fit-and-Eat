f02761cd5523b623e4359f02aa97fdaa43a7cc7b
JungleBird <118018665+RealJungleBird@users.noreply.github.com>
2025-05-04 03:57:05 +0300

    Update DaySummary.kt

--- app/src/main/java/su/junglebird/fiteat/ui/screens/DaySummary.kt
+++ app/src/main/java/su/junglebird/fiteat/ui/screens/DaySummary.kt
@@ -3,10 +3,21 @@ package su.junglebird.fiteat.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DaySummary() {
     Column {
-        Text(text = "Сводка за день")
+        Text(text = "Всего употреблено:")
+        Text(text="168" + " Ккал",
+            fontSize = 35.sp)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    DaySummary()
 }
